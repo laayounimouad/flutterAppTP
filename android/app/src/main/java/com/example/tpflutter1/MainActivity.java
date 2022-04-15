c85f6d07835cce7cd7c2a2cf69a6fde29b52ed66
mouad01laayouni <mouad01laayouni@gmail.com>
2022-04-15 02:43:59 +0000

    first commit

---
+++ android/app/src/main/java/com/example/tpflutter1/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.tpflutter1;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/pages/covid19.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Covid19 extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+  return _Covid19();
+  }
+}
+
+class _Covid19 extends State<Covid19>{
+  var stats;
+  var data;
+  var general;
+  @override
+  void initState(){
+    super.initState();
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      getStats();
+    });
+  }
+  getStats() {
+    String url = "https://api.covid19api.com/summary";
+    http.get(Uri.parse(url))
+        .then((response) {
+      setState(() {
+        Map _json = json.decode(response.body);
+        data=[];
+        _json.values.forEach((element) {
+          if(element!=null ) {
+            //print("$element \n\n");
+            data.add(element);
+          }
+        });
+        //searchCountry(keyword);
+      });
+      general=[];
+      stats=[];
+      general.add(data[2]);
+      stats.add(data[3]);
+//print(data);
+    }).catchError((onError) {
+      print("Error while calling the api ==> " + onError.toString());
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    //getStats();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('covid 19 stats'),
+      ),
+      body: Padding(padding: EdgeInsets.all(10),
+          child: Column(
+                children: [
+                  Card(
+                      child: ListTile(
+                        title: Title(color: Colors.black,
+                            child: Text("World Stats"),
+                        ),
+                        subtitle: Column(
+                          children: [
+                            Text("Total confirmed : ${general[0]["TotalConfirmed"].toString()}"),
+                            Text("Total deaths : ${general[0]["TotalDeaths"].toString()}"),
+                          ],
+                        )
+                        ),
+                  ),
+                  Expanded(child:
+                    ListView.builder(itemCount: stats==null?0:stats[0].length,itemBuilder: (context, index) {
+                    var country = stats[0][index]["Country"];
+                    return Text("Country : $country");
+                  },))
+                ],
+      )
+      ),
+    );
+  }
+
+}
